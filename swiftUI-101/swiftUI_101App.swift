cb6f7471459290321038c3e66a74acde1681bf2f
Julio Perez Viloria <protobot.dev@gmail.com>
2021-11-24 17:26:23 -0300

    Initial Commit

---
+++ swiftUI-101/swiftUI_101App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_101App.swift
+//  swiftUI-101
+//
+//  Created by Julio Perez Viloria on 24/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
